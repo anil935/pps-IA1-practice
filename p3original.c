27fec3c39d6bd64b2e3e3df7a60524554a11354d
anil935 <94883184+anil935@users.noreply.github.com>
2022-02-02 10:53:00 +0000

    p3

--- p3original.c
+++ p3original.c
@@ -1,1 +1,25 @@
-
+#include<stdio.h>
+int input_n()
+{
+  int a;
+  printf("enter a number:\n");
+  scanf("%d",&a);
+  return a;
+}
+int sum_n(int n)
+{
+  int sum;
+  sum=n*(n+1)/2;
+  return sum;
+}
+void output(int sum)
+{
+  printf("sum of natural numbers is: %d", sum);
+}
+int main()
+{
+  int n=input_n();
+  int sum=sum_n(n);
+  output(sum);
+  return 0;
+}
