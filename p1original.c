8ed4c2bed381de4b3404bd1837c4c889f0a3f639
anil935 <94883184+anil935@users.noreply.github.com>
2022-02-02 10:51:24 +0000

    p1

--- p1original.c
+++ p1original.c
@@ -1,6 +1,22 @@
 #include<stdio.h>
+void input(int *a,int *b)
+{
+  printf("enter two numbers:\n");
+  scanf("%d%d",a,b);
+}
+void add(int a,int b,int *sum)
+{
+  *sum=a+b;
+}
+void output(int a,int b,int sum)
+{
+  printf("the sum of %d and %d is %d\n",a,b,sum);
+}
 int main()
 {
-  printf("my name is anil kumar\n");
+  int x,y,z;
+  input(&x,&y);
+  add(x,y,&z);
+  output(x,y,z);
   return 0;
 }
